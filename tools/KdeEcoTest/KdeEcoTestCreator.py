63afea11d02dca02d9af3579ae679e5dfd0d18d6
emmanuel charruau <echarruau@gmail.com>
2022-06-03 10:27:58 +0200

    Removed KdeEcoTestCreator debug lines and fixed os.exit bug

--- tools/KdeEcoTest/KdeEcoTestCreator.py
+++ tools/KdeEcoTest/KdeEcoTestCreator.py
@@ -1,4 +1,3 @@
-# Modify Actiona script to adapt click coordinates to different screen resolutions
 import re
 import argparse
 import os
@@ -8,31 +7,30 @@ from pynput import keyboard
 from pynput.keyboard import Key
 import argparse
 from xdo import Xdo
+import signal
 
 window_defined = False
 writeMousePosToFile = False
-
 
 def defineWindow():
     ## get application origin coordinates
     xdo = Xdo()
     win_id = xdo.select_window_with_click()
+
     global win_location
     win_location = xdo.get_window_location(win_id)
-    print(win_location.x)
-    
-    print(win_location.y)
+
     global window_defined
     window_defined = True
 
     global win_size
     win_size = xdo.get_window_size(win_id)
-    print(win_size.width)
-    print(win_size.height)
 
+    #SetWindowToOriginalSize will need to be used, but at the moment I do not know where. It has to be written because KdeEcoTest has to be applied on the same windows size than the original tested window.
     file1 = open(outputFilename, 'a')
     file1.write("# Original window properties\n")
-    file1.write("moveWindowToOriginalLocation {0},{1}\n".format(win_location.x,win_location.y))
+    #I comment moveWindowToOriginalLocation at the moment because I am not sure it is useful.
+    #file1.write("moveWindowToOriginalLocation {0},{1}\n".format(win_location.x,win_location.y))
     file1.write("setWindowToOriginalSize {0},{1}\n\n".format(win_size.width,win_size.height))
     file1.close()
 
@@ -63,7 +61,6 @@ def writeToScreen():
     file1.write("sleep 2\n")
     file1.write("\n")
     file1.close()
-    
 
 
 #get input arguments
@@ -75,14 +72,8 @@ outputFilename = args.outputFilename
 
 def on_click(x, y, button, pressed):
     if writeMousePosToFile:
-        print("tttttttttttttttttttttt1")
         if button == mouse.Button.left:
             if pressed:
-                print("test")
-                print(x)
-                print(win_location.x)
-                #+ win_size.width)
-
                 if ((x > win_location.x + win_size.width) or (y > win_location.y + win_size.height)):
                     print("Click outside window, do not record click")
                     return
@@ -90,12 +81,8 @@ def on_click(x, y, button, pressed):
                 print("mouse click position added to the file")
                 global windows_x
                 windows_x = x - win_location.x
-                print(windows_x)
                 global windows_y
                 windows_y = y - win_location.y
-                print("win_location: ")
-                print(win_location)
-                print("test: " + outputFilename)
                 file1 = open(outputFilename, 'a')
                 clickOnMsgStr = 'click {0},{1}'.format(windows_x, windows_y)
                 sleepMsgStr = 'sleep {0}'.format(2)
@@ -103,6 +90,7 @@ def on_click(x, y, button, pressed):
                 file1.write(clickOnMsgStr + "\n")
                 file1.write(sleepMsgStr + "\n")
                 file1.write("\n")
+                print("# Click on\n")
                 print(clickOnMsgStr + "\n")
                 print(sleepMsgStr + "\n")
                 print("\n")
@@ -123,15 +111,11 @@ print("\n")
 
 print("To begin with, click on the application you want the script to be written for.")
 defineWindow()
-print("window_defined: ")
-print(window_defined)
-print("\n")
 
 
 while True:
     print("Enter your command: ")
     commandStr = input()
-    
  
     if commandStr == "dw":
         defineWindow()
@@ -142,24 +126,7 @@ while True:
     elif commandStr == "sc":
         stopClick()
     elif commandStr == "q":
-        os._exit()
+        os.kill(os.getpid(), signal.SIGTERM)
     else:
         print("Command unknown.")
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
